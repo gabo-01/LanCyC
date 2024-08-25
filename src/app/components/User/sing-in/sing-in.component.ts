5d53e6f7caff50e070c2c11f4894d774e2455dc4
Fernando Martinez <gabrielmtzg76@gmail.com>
2024-08-24 20:14:52 -0600

    Validation form login and signin

--- src/app/components/User/sing-in/sing-in.component.ts
+++ src/app/components/User/sing-in/sing-in.component.ts
@@ -1,16 +1,24 @@
-  import { Component, ElementRef, ViewChild } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
   import { CommonModule } from '@angular/common';
+import { FormGroup ,FormControl,Validators, ReactiveFormsModule} from '@angular/forms';
   @Component({
     selector: 'app-sing-in',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule,ReactiveFormsModule],
     templateUrl: './sing-in.component.html',
     styleUrl: './sing-in.component.css'
   })
-  export class SingInComponent {
+  export class SingInComponent implements OnInit {
 
+    formulario:FormGroup= new FormGroup({});
     fieldTextType: boolean= false;
    
+    ngOnInit(): void {
+        this.formulario=new FormGroup({
+          username: new FormControl('',[ Validators.required,Validators.email]),
+          password: new FormControl('',[ Validators.required,Validators.minLength(8)]),
+        })
+    }
 
     constructor(){
 
